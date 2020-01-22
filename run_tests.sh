f6d8fdd4e398adfe0074c2e5f2e935bc06c856e4
Andy Clifton <andy_clifton@yahoo.com>
2020-01-22 21:03:01 +0100

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -16,16 +16,18 @@ echo "Finished step 1."
 echo " ".
 
 # get a list of directories in "tests"
-DIRECTORIES=$(find ${PWD}/tests/ -mindepth 1 -type d -regex '\./[^\.].*')
+echo "2. checking for directories in ${PWD}..."
+DIRECTORIES=$(find ${PWD}/tests/ -maxdepth 1 -type d -regex '\./[^\.].*')
 
 # copy style file in to them
-echo "2. Copying corporate.sty to tests directories..."
+echo "3. Copying corporate.sty to tests directories..."
 for d in $DIRECTORIES
 do
+  echo "... copying files to $d."
   cp corporate.sty $d
   echo "... updated files in $d."
 done
-echo "Finished step 2."
+echo "Finished step 3."
 echo " ".
 
 # run tests: loop through each directory that we found earlier.
@@ -33,16 +35,20 @@ echo "3. Compiling test documents"
 for d in $DIRECTORIES
 do
   cd $d
-  echo "...running tests in $d"
+  echo "...running tests on latex document in $d"
   echo "-------------------"
   FILES=$(find . -iname '*.tex')
 	for f in $FILES
 		do
-			if [-z "$f"]
-			else
 				filename=$(basename -- "$f")
 				extension="${filename##*.}"
 				filename="${filename%.*}"
+
+			if [-z "$filename"]
+			then
+				echo "...filename $filename not valid ..."
+			else
+				# 1. Run latex on the documents
 				echo "...processing $filename using LaTeX ..."
 				find $filename.* -type f ! -name "$filename.tex" ! -name "$filename.bib" -exec rm -f {} +
 				pdflatex -shell-escape -halt-on-error -interaction=nonstopmode $f
