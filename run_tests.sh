5df5dd176880436da6d3873d94867ed995ecab16
Andy C <andy_clifton@yahoo.com>
2020-03-03 21:51:24 +0100

    FIX: Repo structure

--- run_tests.sh
+++ run_tests.sh
@@ -5,28 +5,18 @@
 echo "Running test suite in ${PWD}..."
 echo " "
 
-echo "0. Copying source/corporateIdentity.sty to root"
+echo "1. Copying source/corporateIdentity.sty to root"
 cp source/corporateIdentity.sty ${PWD}
 cp source/corporateconfig.tex ${PWD}
 echo "...copied source/corporateIdentity.sty to $PWD."
 echo "Finished step 0."
 echo " "
 
-echo "1. Checking for old tests"
-if [ -d tests_old/ ]; then
-  rm -rf tests_old/
-  echo "...removed existing old tests."
-fi
-cp -RP tests tests_old/
-echo "...copied current tests to old tests."
-echo "Finished step 1."
-echo " "
-
 # get a list of directories in "tests"
-echo "2. checking for subdirectories containing *test.tex files in ${PWD}/tests ..."
+echo "2. checking for subdirectories containing test-*.tex files in ${PWD}/tests ..."
 #DIRECTORIES=$(find ${PWD}/tests -name '*test.tex' -printf "%h\n" | sort -u)
 # Find the files we are interested in
-DIRECTORIES=$(find ${PWD}/tests -iname '*test.tex' | sed 's|/[^/]*$||' | sort -u)
+DIRECTORIES=$(find ${PWD}/tests -iname 'test-*.tex' | sed 's|/[^/]*$||' | sort -u)
 
 # loop through them
 for d in $DIRECTORIES
@@ -51,7 +41,7 @@ do
   cd $d
   echo "... checking directory $d for test files"
   echo "-------------------"
-  TEXMAINFILES=$(find $d -iname '*test.tex' | sort -u)
+  TEXMAINFILES=$(find $d -iname 'test-*.tex' | sort -u)
   for f in $TEXMAINFILES
   do
       TEXMAINFILE=$(basename $f .tex)
